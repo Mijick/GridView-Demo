d1ca3d9a3cb2d79b38de5334c7732ebdad50c488
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-21 00:20:51 +0200

    Initial Commit

---
+++ GridView Demo/GridView_DemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  GridView_DemoApp.swift of GridView Demo
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+@main
+struct GridView_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
